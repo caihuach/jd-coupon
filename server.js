46a655c887c854dae805f05a2d5e5d4102fccda8
ch <caihua90@qq.com>
2019-07-02 23:15:09 +0800

    finish parsing first page

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const axios = require('axios');
 const puppeteer = require('puppeteer-core');
 
 
-const url = 'https://search.jd.com/Search?coupon_batch=233216830&coupon_id=36729135679';
+const url = 'https://search.jd.com/Search?coupon_batch=236566802&coupon_id=68970403689';
 
 
 async function main() {
   //用特定尺寸打开浏览器，并且viewport跟随浏览器尺寸
   const browser = await puppeteer.launch({
     headless: false,
-    executablePath: 'C:\\Users\\lenovo\\AppData\\Local\\CentBrowser\\Application\\chrome.exe',
+    executablePath: 'C:\\Users\\ch\\AppData\\Local\\CentBrowser\\Application\\chrome.exe',
     defaultViewport: null,
     args: ['--window-size=1600,900']
   });
@@ -17,6 +17,25 @@ async function main() {
   const page = await browser.newPage();
   await page.goto(url, {waitUntil: 'networkidle2'});
 
+  //todo:怎么翻页
+  //while loop，按右键可以翻页，最后一页就会disabled
+
+
+  //翻到最底部，触发所有的autoload
+  await autoScroll(page);
+
+  const rows = getRows4thisPage(page);
+
+  console.dir(rows);
+
+  // await browser.close();
+}
+
+
+main();
+
+
+async function autoScroll(page) {
   await page.evaluate(async () => {
     await new Promise((resolve, reject) => {
       let totalHeight = 0;
@@ -33,26 +52,53 @@ async function main() {
       }, 200)
     })
   });
+}
 
-  const rows = await page.evaluate(() => {
+async function getRows4thisPage(page) {
+  return await page.evaluate(() => {
     const lis = document.querySelectorAll('li.gl-item');
     console.log(lis.length);
 
 
-    const list = [];
-    const items = document.querySelectorAll('li.gl-item i[data-tips="本商品参与满减促销"]');
+    const rows = [];
+    const nodeList = document.querySelectorAll('li.gl-item i[data-tips="本商品参与满减促销"]');
 
-    for (const item of items) {
-      console.log(item.innerHTML);
+    //遍历所有打折商品
+    for (const node of nodeList) {
+      console.dir(node);
+      const {
+        parentNode,
+        innerText
+      } = node;
+      const {children} = parentNode.parentNode;
+
+      //找到有href的pName
+      let pName;
+      for (const child of children) {
+        if (child.className.includes('p-name')) {
+          pName = child;
+          break;
+        }
+      }
+      if (!pName) {
+        continue;
+      }
+      //找到href那个元素
+      let href;
+      for (const child of pName.children) {
+        if (child.tagName === 'A') {
+          href = child.href;
+          rows.push({
+            href,
+            name: child.innerText,
+            discount: innerText
+          });
+          break;
+        }
+      }
     }
 
 
-    return list;
+    return rows;
   });
-
-
-  // await browser.close();
 }
-
-
-main();
