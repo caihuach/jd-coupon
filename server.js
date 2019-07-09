dc925fd3274267b1d6805688e33424a570c4e216
ch <caihua90@qq.com>
2019-07-10 00:24:43 +0800

    能翻页，有最终物件。还需要加入价格属性。

--- server.js
+++ server.js
@@ -1,39 +1,9 @@
-const axios = require('axios');
 const puppeteer = require('puppeteer-core');
 
+const discountObj = {};
+let discountArr = [];
 
-const url = 'https://search.jd.com/Search?coupon_batch=236566802&coupon_id=68970403689';
-
-
-async function main() {
-  //用特定尺寸打开浏览器，并且viewport跟随浏览器尺寸
-  const browser = await puppeteer.launch({
-    headless: false,
-    executablePath: 'C:\\Users\\ch\\AppData\\Local\\CentBrowser\\Application\\chrome.exe',
-    defaultViewport: null,
-    args: ['--window-size=1600,900']
-  });
-
-  const page = await browser.newPage();
-  await page.goto(url, {waitUntil: 'networkidle2'});
-
-  //todo:怎么翻页
-  //while loop，按右键可以翻页，最后一页就会disabled
-
-
-  //翻到最底部，触发所有的autoload
-  await autoScroll(page);
-
-  const rows = getRows4thisPage(page);
-
-  console.dir(rows);
-
-  // await browser.close();
-}
-
-
-main();
-
+const url = 'https://search.jd.com/Search?coupon_batch=239432678&coupon_id=37426841159';
 
 async function autoScroll(page) {
   await page.evaluate(async () => {
@@ -54,8 +24,8 @@ async function autoScroll(page) {
   });
 }
 
-async function getRows4thisPage(page) {
-  return await page.evaluate(() => {
+function getRows4thisPage(page) {
+  return page.evaluate(() => {
     const lis = document.querySelectorAll('li.gl-item');
     console.log(lis.length);
 
@@ -68,7 +38,7 @@ async function getRows4thisPage(page) {
       console.dir(node);
       const {
         parentNode,
-        innerText
+        innerText: discountName
       } = node;
       const {children} = parentNode.parentNode;
 
@@ -91,14 +61,91 @@ async function getRows4thisPage(page) {
           rows.push({
             href,
             name: child.innerText,
-            discount: innerText
+            discountName,
           });
           break;
         }
       }
     }
 
-
     return rows;
   });
 }
+
+function nextPageDisabled(page) {
+  return page.evaluate(() => {
+    const next = document.querySelector('.pn-next');
+    if (!next) {
+      return true;
+    }
+    return next.className.includes('disabled');
+  })
+}
+
+function convertArr(discountArr) {
+  for (const discount of discountArr) {
+    const {
+      href,
+      name,
+      discountName
+    } = discount;
+
+    if (!Array.isArray(discountObj[discountName])) {
+      discountObj[discountName] = [];
+    }
+    discountObj[discountName].push({
+      href,
+      name,
+    });
+  }
+}
+
+async function main() {
+  //用特定尺寸打开浏览器，并且viewport跟随浏览器尺寸
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: 'C:\\Users\\ch\\AppData\\Local\\CentBrowser\\Application\\chrome.exe',
+    defaultViewport: null,
+    args: ['--window-size=1600,900']
+  });
+
+  const page = await browser.newPage();
+  await page.goto(url, {waitUntil: 'networkidle2'});
+
+
+  //todo:怎么翻页
+  //while loop，按右键可以翻页，最后一页就会disabled
+
+  while (true) {
+    //1. 翻到最底部，触发所有的autoload
+    await autoScroll(page);
+    //2. 看看有没有打折的
+    const rows = await getRows4thisPage(page);
+
+    //3. 存起来这些rows
+    discountArr = discountArr.concat(rows);
+
+    //4. 看看下一页还有没有了
+    const shouldBreak = await nextPageDisabled(page);
+
+    console.log(shouldBreak);
+    if (shouldBreak) {
+      break;
+    }
+    //5. 翻页
+    await Promise.all([
+      page.waitForNavigation({waitUntil: 'networkidle2'}),
+      page.keyboard.press('ArrowRight'),
+    ])
+  }
+
+
+  convertArr(discountArr);
+  console.dir(discountObj);
+
+
+  // await browser.close();
+}
+
+
+main();
